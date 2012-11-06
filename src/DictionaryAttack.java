537a81c659b2a69b5d1809cc34398101ccb7a1dc
Le Tan Khoa <tkhoa2711@yahoo.com.vn>
2012-11-06 16:41:56 +0800

    added getHash() and attack() functions

--- src/DictionaryAttack.java
+++ src/DictionaryAttack.java
@@ -1,8 +1,35 @@
+import java.security.MessageDigest;
+import java.io.*;
 
+public class DictionaryAttack {
 
-public class Main {
+	public static String getHash(String text) throws NoAlgorithmException {
+		MessageDigest digest = MessageDigest.getInstance("SHA-1");
+		digest.reset();
+		return digest.digest(text.getBytes("UTF-8")).toString();
+	}
+	
+	public static String attack(String dictionaryFileName, String hashString) {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(dictionaryFileName));
+			String line = "";
+			while((line = br.readLine()) != null) {
+				if(getHash(line).equalsIgnoreCase(hashString)) {				
+					break;
+				}
+			}
+			br.close();
+			return line;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public static void main(String args[]) {
-		
+		String dict_file = "data/dictionary.txt";
+		String hash = "";
+		String text = attack(dict, hash);
+		System.out.println("Hash = " + hash);
+		System.out.println("Original text = " + text);
 	}
 }
